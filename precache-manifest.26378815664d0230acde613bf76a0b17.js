84b54d4ed4508ddd674ea4f48b05eb58d3ed6ab8
martajablonska <martawaldon@gmail.com>
2018-12-06 11:47:34 +0100

    Updates

---
+++ precache-manifest.26378815664d0230acde613bf76a0b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08885671e95ced490f8",
+    "url": "/Weather_app/static/js/runtime~main.b0888567.js"
+  },
+  {
+    "revision": "a78adaee65241d1e0ff8",
+    "url": "/Weather_app/static/js/main.a78adaee.chunk.js"
+  },
+  {
+    "revision": "81cadd10a82d36be19e4",
+    "url": "/Weather_app/static/js/1.81cadd10.chunk.js"
+  },
+  {
+    "revision": "a78adaee65241d1e0ff8",
+    "url": "/Weather_app/static/css/main.54b5070d.chunk.css"
+  },
+  {
+    "revision": "c09e0459a50dada068e8f404df0ea92b",
+    "url": "/Weather_app/index.html"
+  }
+];
